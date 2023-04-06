385856c2649a0e594f7c30bebc82e272356d2dc2
calebeLAR <ca6ele@gmail.com>
2023-04-06 13:32:26 -0400

    abrindo o PR

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-// ...
+// ....
 
 const app = express();
 
@@ -11,7 +11,7 @@ app.get('/', (_request, response) => {
 
 app.use(express.json());
 
-// ...
+// ....
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
