023866d72a458062cb3f532c1b0294e75c94d73c
calebeLAR <ca6ele@gmail.com>
2023-04-30 19:59:37 -0400

    cria migrations de users

---
+++ src/migrations/20230430230756-create-users.js
@@ -0,0 +1,34 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        type: Sequelize.INTEGER,
+      },
+      displayName: {
+        allowNull: false,
+        field: 'display_name',
+        type: Sequelize.STRING,
+      },
+      email: {
+        unique: true,
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      image: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
