76ff88b677ec55f16317e790e2498f324a987beb
calebeLAR <ca6ele@gmail.com>
2023-05-01 18:19:40 -0400

    feat: cria userService com a função createUser

---
+++ src/services/user.service.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const { User } = require('../models');
+
+const secret = process.env.JWT_SECRET || 'secretJWT';
+const algorithm = { algorithm: 'HS256', expiresIn: '7d' };
+
+const createUser = async (user) => {
+  try {
+    const isAlreadyRegistered = await User.findOne({ where: { email: user.email } });
+    if (isAlreadyRegistered) {
+      return { type: 'USER_ALREADY_REGISTERED', message: 'User already registered' };
+    }
+
+    const userCreated = await User.create(user);
+
+    const { password: _, ...userWithOutPassword } = userCreated.dataValues;
+
+    const token = jwt.sign({ payload: userWithOutPassword }, secret, algorithm);
+
+    return { type: null, message: token };
+  } catch (err) {
+    return { type: 'INTERNAL_ERROR', message: err.message };
+  }
+};
+
+module.exports = {
+  createUser,
+};
