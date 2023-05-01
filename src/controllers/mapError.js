0fcb96daff372d3249fd328ee2438d4910724ff1
calebeLAR <ca6ele@gmail.com>
2023-05-01 14:51:11 -0400

    feat: cria arquivo que mapeia status code de error

---
+++ src/controllers/mapError.js
@@ -0,0 +1,13 @@
+const errorMap = {
+  INVALID_VALUE: 400,
+  MISSING_VALUE: 400,
+  USER_NOT_FOUND: 400,
+};
+
+const mapErros = (type) => {
+  const status = errorMap[type] || 500;
+  
+  return status;
+};
+
+module.exports = mapErros;
