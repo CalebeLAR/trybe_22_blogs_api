45bfbbf77d96c5e5ecd2c46a71f92730b3073ecb
calebeLAR <ca6ele@gmail.com>
2023-05-01 09:52:30 -0400

    feat: cria model user

---
+++ src/models/User.js
@@ -0,0 +1,47 @@
+/**
+ * 
+ * @param {import('sequelize').Sequelize} Sequelize 
+ * @param {import('sequelize').DataTypes} DataTypes 
+ */
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+    },
+    displayName: {
+      allowNull: false,
+      // field: 'display_name',
+      type: DataTypes.STRING,
+    },
+    email: {
+      unique: true,
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    image: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    }
+  },{
+    tableName: 'users',
+    timestamps: false,
+    underscored: true,
+  });
+
+  User.associate = (model) => {
+    User.hasMany(model.BlogPosts,{
+      foreignKey: 'user_id',
+      as: 'blogPosts'
+    });
+  }
+
+  return User;
+}
