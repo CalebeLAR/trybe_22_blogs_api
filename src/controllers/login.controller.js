a9218b3c7bee22aa2f1428844e4d7b69b793ec05
calebeLAR <ca6ele@gmail.com>
2023-05-01 14:48:40 -0400

    feat: cria loginController que faz um login

---
+++ src/controllers/login.controller.js
@@ -0,0 +1,23 @@
+const { loginValidate } = require('./validations/login.schema.validation');
+const loginService = require('../services/login.service');
+const mapError = require('./mapError');
+
+const login = async (req, res) => {
+  try {
+    const error = loginValidate(req.body);
+    if (error.type) return res.status(mapError(error.type)).json({ message: error.message });
+  
+    const { email, password } = req.body;
+    const { type, message } = await loginService.login(email, password);
+    
+    if (type) return res.status(mapError(type)).json({ message });
+    
+    return res.status(200).json({ token: message });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+  };
+
+module.exports = {
+  login,
+};
