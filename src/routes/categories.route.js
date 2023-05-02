152e7f7aeaed142674c98e4a985109cc2508bb9e
calebeLAR <ca6ele@gmail.com>
2023-05-02 09:45:07 -0400

    feat: cria endpoint post /categories

---
+++ src/routes/categories.route.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const categoriesController = require('../controllers/categories.controller');
+const validateToken = require('../middleware/validateToken');
+
+const categoriesRoute = express.Router();
+
+categoriesRoute.post('/', validateToken, categoriesController.createCategory);
+
+module.exports = categoriesRoute;
