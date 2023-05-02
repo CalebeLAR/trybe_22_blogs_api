e4b89070a48fa7745a63c6c0b1ddb1659cbbb62c
calebeLAR <ca6ele@gmail.com>
2023-05-02 01:26:25 -0400

    refat: usa middleware no endPoint get /user

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const userController = require('../controllers/user.controller');
+const validateToken = require('../middleware/validateToken');
 
 const userRouter = express.Router();
 
 userRouter.post('/', userController.createUser);
-userRouter.get('/', userController.getUsers);
+userRouter.get('/', validateToken, userController.getUsers);
 
 module.exports = userRouter;
