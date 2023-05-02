bbdca5af910f84373e0d54d4da2224e5b7ca0784
calebeLAR <ca6ele@gmail.com>
2023-05-01 21:39:43 -0400

    feat: cria função getUsers e invalidTokeErros

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,17 +1,57 @@
+const jwt = require('jsonwebtoken');
 const { validateUser } = require('./validations/user.schema.validation');
 const { userService } = require('../services');
 const mapError = require('./mapError');
 
-const createUser = async (req, res) => { 
-  const error = validateUser(req.body);
-  if (error.type) return res.status(mapError(error.type)).json({ message: error.message });
+const secret = process.env.JWT_SECRET || 'secretJWT';
 
-  const { type, message } = await userService.createUser(req.body);
-  if (type) return res.status(mapError(type)).json({ message });
+const invalidTokenErros = (err) => {
+  const { message, name } = err;
+  console.log(name);
+  if (message === 'jwt must be provided') {
+    return { type: 'UNAUTHORIZED', message: 'Token not found' };
+  }
+  // message === 'jwt expired' || message === 'invalid token'
+  if (name === 'JsonWebTokenError') {
+    return { type: 'UNAUTHORIZED', message: 'Expired or invalid token' };
+  }
 
-  res.status(201).json({ token: message });
+  return { type: null };
+};
+
+const createUser = async (req, res) => {
+  try {
+    const error = validateUser(req.body);
+    if (error.type) return res.status(mapError(error.type)).json({ message: error.message });
+    
+    const { type, message } = await userService.createUser(req.body);
+    if (type) return res.status(mapError(type)).json({ message });
+    
+    res.status(201).json({ token: message });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
+
+const getUsers = async (req, res) => {
+  try {
+    const { authorization: token } = req.headers;
+
+    jwt.verify(token, secret);
+
+    const { type, message } = await userService.getUsers();
+    if (type) return res.status(mapError(type)).json({ message });
+    
+    return res.status(200).json(message);
+  } catch (err) {
+    const { type, message } = invalidTokenErros(err);
+    if (type) return res.status(mapError(type)).json({ message });
+    
+    return res.status(500).json({ message: err.message });
+  }
 };
 
 module.exports = {
   createUser,
+  getUsers,
 };
