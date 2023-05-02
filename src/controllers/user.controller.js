0b311f0ca80898916c62a81571b24b30d7175d6c
calebeLAR <ca6ele@gmail.com>
2023-05-02 01:25:14 -0400

    refat: userController com funções de autorização

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,23 +1,6 @@
-const jwt = require('jsonwebtoken');
 const { validateUser } = require('./validations/user.schema.validation');
 const { userService } = require('../services');
 const mapError = require('./mapError');
-
-const secret = process.env.JWT_SECRET || 'secretJWT';
-
-const invalidTokenErros = (err) => {
-  const { message, name } = err;
-  console.log(name);
-  if (message === 'jwt must be provided') {
-    return { type: 'UNAUTHORIZED', message: 'Token not found' };
-  }
-  // message === 'jwt expired' || message === 'invalid token'
-  if (name === 'JsonWebTokenError') {
-    return { type: 'UNAUTHORIZED', message: 'Expired or invalid token' };
-  }
-
-  return { type: null };
-};
 
 const createUser = async (req, res) => {
   try {
@@ -27,26 +10,19 @@ const createUser = async (req, res) => {
     const { type, message } = await userService.createUser(req.body);
     if (type) return res.status(mapError(type)).json({ message });
     
-    res.status(201).json({ token: message });
+    return res.status(201).json({ token: message });
   } catch (err) {
     return res.status(500).json({ message: err.message });
   }
 };
 
-const getUsers = async (req, res) => {
+const getUsers = async (_req, res) => {
   try {
-    const { authorization: token } = req.headers;
-
-    jwt.verify(token, secret);
-
     const { type, message } = await userService.getUsers();
     if (type) return res.status(mapError(type)).json({ message });
     
     return res.status(200).json(message);
   } catch (err) {
-    const { type, message } = invalidTokenErros(err);
-    if (type) return res.status(mapError(type)).json({ message });
-    
     return res.status(500).json({ message: err.message });
   }
 };
