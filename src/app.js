4de91589e4daf37953fd15650a8e012a53c5558a
calebeLAR <ca6ele@gmail.com>
2023-05-02 14:06:12 -0400

    feat: cria rota de posts

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { loginRoute, userRoute, categoriesRoute } = require('./routes');
+const { loginRoute, userRoute, categoriesRoute, postRoute } = require('./routes');
 
 // ....
 
@@ -16,6 +16,7 @@ app.use(express.json());
 app.use('/login', loginRoute);
 app.use('/user', userRoute);
 app.use('/categories', categoriesRoute);
+app.use('/post', postRoute);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
