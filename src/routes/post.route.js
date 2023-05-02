7d1050f7d8373b346718cbc403e23c9903912250
calebeLAR <ca6ele@gmail.com>
2023-05-02 14:09:12 -0400

    feat: cria router e endpoint /post para posts

---
+++ src/routes/post.route.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const validateToken = require('../middleware/validateToken');
+const postController = require('../controllers/post.controller');
+
+const postRoute = express.Router();
+
+postRoute.post('/', validateToken, postController.createBlogPost);
+
+module.exports = postRoute;
