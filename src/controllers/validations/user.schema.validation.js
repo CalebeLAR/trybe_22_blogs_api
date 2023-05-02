8c24137830ff48f77fbfca2c83b4c70835ae8518
calebeLAR <ca6ele@gmail.com>
2023-05-02 08:25:40 -0400

    feat: cria o validador de userId

--- src/controllers/validations/user.schema.validation.js
+++ src/controllers/validations/user.schema.validation.js
@@ -1,4 +1,4 @@
-const { userShema } = require('./user.schema');
+const { userShema, idUserShema } = require('./user.schema');
 
 const validateUser = (user) => {
   const { error } = userShema.validate(user);
@@ -13,6 +13,20 @@ const validateUser = (user) => {
   return { type: null, message: user };
 };
 
+const validateUserId = (userId) => {
+  const { error } = idUserShema.validate(userId);
+  if (error && error.message.includes('required')) {
+    return { type: 'MISSING_VALUE', message: error.message };
+  }
+
+  if (error) {
+    return { type: 'INVALID_VALUE', message: error.message };
+  }
+
+  return { type: null, message: userId };
+};
+
 module.exports = {
   validateUser,
+  validateUserId,
 };
