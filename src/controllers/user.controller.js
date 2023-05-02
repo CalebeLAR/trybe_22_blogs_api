8e9c9eccf4a0c6fb9f156bf1a732a7ded65371d1
calebeLAR <ca6ele@gmail.com>
2023-05-02 08:24:35 -0400

    feat: cria controller getUser pra listar um user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,4 @@
-const { validateUser } = require('./validations/user.schema.validation');
+const { validateUser, validateUserId } = require('./validations/user.schema.validation');
 const { userService } = require('../services');
 const mapError = require('./mapError');
 
@@ -16,9 +16,25 @@ const createUser = async (req, res) => {
   }
 };
 
-const getUsers = async (_req, res) => {
+const getUsers = async (req, res) => {
   try {
     const { type, message } = await userService.getUsers();
+    if (type) return res.status(mapError(type)).json({ message });
+    
+    return res.status(200).json(message);
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
+
+const getUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const error = validateUserId(id);
+    if (error.type) return res.status(mapError(error.type)).json({ message: error.message });
+
+    const { type, message } = await userService.getUser(id);
     if (type) return res.status(mapError(type)).json({ message });
     
     return res.status(200).json(message);
@@ -30,4 +46,5 @@ const getUsers = async (_req, res) => {
 module.exports = {
   createUser,
   getUsers,
+  getUser,
 };
